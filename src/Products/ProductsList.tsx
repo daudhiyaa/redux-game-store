1976bbda6c177f7e2b86e6303a9ea0c260badea4
Daud Dhiya <daud.dhiya31@gmail.com>
2024-03-11 21:25:38 +0700

    feat: connecting React component to Redux store with useSelector

--- src/Products/ProductsList.tsx
+++ src/Products/ProductsList.tsx
@@ -1,31 +1,8 @@
-import { useState } from "react";
-
-interface Product {
-  id: number;
-  title: string;
-  price: number;
-}
-
-const initialProducts = [
-  {
-    id: 1,
-    title: "Super Mario Bros",
-    price: 60,
-  },
-  {
-    id: 2,
-    title: "Legend of Zelda",
-    price: 60,
-  },
-  {
-    id: 3,
-    title: "Metroid",
-    price: 60,
-  },
-];
+import { useSelector } from "react-redux";
+import { getProductsSelector } from "./products.slice";
 
 const ProductList = () => {
-  const [products, setProducts] = useState<Product[]>(initialProducts);
+  const products = useSelector(getProductsSelector);
 
   return (
     <div>
@@ -35,21 +12,6 @@ const ProductList = () => {
           {product.title} : ${product.price}
         </div>
       ))}
-
-      <button
-        onClick={() =>
-          setProducts((prevProducts) => [
-            {
-              id: 4,
-              title: "New Game 1",
-              price: 60,
-            },
-            ...prevProducts,
-          ])
-        }
-      >
-        Add Product
-      </button>
     </div>
   );
 };
